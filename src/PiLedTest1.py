82cc24ce6e7203c04afe9ecceabd882387b6aa6e
Xavier <xaviersiao.23@ichat.sp.edu.sg>
2024-05-09 20:45:10 +0800

    Updated

--- src/PiLedTest1.py
+++ src/PiLedTest1.py
@@ -5,6 +5,14 @@ from time import sleep
 from hal import hal_input_switch as in_sw
 from hal import hal_led as led
 
+def blink_length_10Hz(seconds):
+    led.init()
+    length = seconds * 10
+    for i in range(length):
+        led.set_output(0,1)
+        time.sleep(0.1)
+
+
 
 def led_blinker():
     i = True
@@ -14,7 +22,7 @@ def led_blinker():
 
         i = True
         switch = in_sw.read_slide_switch()
-        while switch == 1:
+        while switch == 1: #switch is to the left
             led.set_output(0, 1)
             time.sleep(1)
 
@@ -25,8 +33,9 @@ def led_blinker():
 
                     
 
-        while switch == 0:
-            led.set_output(0, 0) 
+        while switch == 0:  #switch is to the right
+            blink_length_10Hz(5) 
+            led.set_output(0,0)
             time.sleep(2)
             switch = in_sw.read_slide_switch()
         switch = in_sw.read_slide_switch()
